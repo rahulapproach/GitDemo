2b0e0cd831fbe250f0a6145fa28cf6a2fa8e1eba
rahul <rktheonly@gmail.com>
2020-04-03 13:27:19 +0530

    first commit

---
+++ src/test/java/demoproject/AppiumTest.java
@@ -0,0 +1,18 @@
+package demoproject;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest 
+{
+	@Test
+	public void test3()
+	{
+		System.out.println("print test3");
+	}
+
+	@Test
+	public void test4() 
+	{
+		System.out.println("print test4");
+	}
+}
---
+++ src/test/java/demoproject/SeleniumTest.java
@@ -0,0 +1,19 @@
+package demoproject;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest 
+{
+	@Test
+	public void test1() 
+	{
+		System.out.println("print test1");
+	}
+
+	
+	@Test
+	public void test2()
+	{
+		System.out.println("print test2");
+	}
+}
